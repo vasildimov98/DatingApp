7fd33377e1cd8ffc015eb4c7996aacd53c0bf7aa
Vasil Dimov <vasil.dimov@accedia.com>
2024-05-31 14:09:45 +0300

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(ApplicationDbContext dbContext) : ControllerBase
+{
+    private readonly ApplicationDbContext dbcontext = dbContext;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await this.dbcontext.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await this.dbcontext.Users.FindAsync(id);
+
+        return user;
+    }
+}
---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options)
+     : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
