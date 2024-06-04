d92eb06dc826dfc52a982bb7c9f7f65412bca724
Vasil Dimov <vasil.dimov@accedia.com>
2024-06-04 14:08:02 +0300

    Create walking skeleton Angular

--- API/Program.cs
+++ API/Program.cs
@@ -8,13 +8,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
