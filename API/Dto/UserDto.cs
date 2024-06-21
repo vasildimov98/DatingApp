7878b88b15230d6f85fba4082ae50990fd376372
Vasil Dimov <vasil.dimov@accedia.com>
2024-06-21 14:26:33 +0300

    Start from scratch because of major update

--- API/Dto/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Dto;
-
-public class UserDto
-{
-    public string Username { get; set; }
-
-    public string Token { get; set; }
-}
--- API/Services/ITokenService.cs
+++
@@ -1,8 +0,0 @@
-﻿using API.Entities;
-
-namespace API.Services;
-
-public interface ITokenService
-{
-    string GenerateToken(AppUser user);
-}
