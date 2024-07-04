d69c2092b47a7f9cedbf6b2b3b57b49445705bb8
Vasil Dimov <vasil.dimov@accedia.com>
2024-07-04 11:26:44 +0300

    End of Section 5 | Client Login and Register

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  imports: [RegisterComponent],
+})
+export class HomeComponent implements OnInit {
+  private readonly http = inject(HttpClient);
+
+  registerMode = false;
+  users: any;
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegister(event: boolean) {
+    this.registerMode = event;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed successfully!!!'),
+    });
+  }
+}
